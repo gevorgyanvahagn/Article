6381bf8052bc740d97276825764d84376a8961e9
Vahagn Gevorgyan <vahagngevorgyan@icloud.com>
2018-12-30 01:17:09 +0400

    Initial Commit

---
+++ The Guardian/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Guardian
+//
+//  Created by Vahagn Gevorgyan on 30/12/2018.
+//  Copyright © 2018 Vahagn Gevorgyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
