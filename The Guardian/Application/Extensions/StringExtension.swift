a4b3b11faa893dd815af29db4d892cb28d89584c
Vahagn Gevorgyan <vahagngevorgyan@icloud.com>
2019-01-01 23:29:43 +0400

    TODO-s are done.

--- The Guardian/Application/Extensions/StringExtension.swift
+++ The Guardian/Application/Extensions/StringExtension.swift
@@ -24,4 +24,23 @@ extension String {
     var readingDuration: Int {
         return Int(count / 500)
     }
+    
+    var words: [String] {
+        return components(separatedBy: .punctuationCharacters)
+            .joined()
+            .components(separatedBy: .whitespaces)
+            .filter{!$0.isEmpty}
+    }
+    
+    func matchesCount(of string: String) -> Int {
+        
+        do {
+            let regex = try NSRegularExpression(pattern: "(?<=\\b)\(string)(?=\\b)", options: .caseInsensitive)
+            
+            let matches = regex.matches(in: self, options: NSRegularExpression.MatchingOptions.reportProgress , range: NSRange(location: 0, length: self.utf16.count)) as [NSTextCheckingResult]
+            return matches.count
+        } catch {
+            return 0
+        }
+    }
 }
