c290a570f3a4b862a4054356db3b9320edec5b11
Vahagn Gevorgyan <vahagngevorgyan@icloud.com>
2019-08-28 00:01:31 +0400

    Update to swift 5.

---
+++ ArticleTests/ArticleTests.swift
@@ -0,0 +1,24 @@
+//
+//  ArticleTests.swift
+//  ArticleTests
+//
+//  Created by Vahagn Gevorgyan on 27.08.2019.
+//  Copyright © 2019 Vahagn Gevorgyan. All rights reserved.
+//
+
+import XCTest
+
+class ArticleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        XCTAssertEqual(10 / 2, 20 / 4)
+    }
+}
